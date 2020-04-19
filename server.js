95622aa42908ab9733e713989af39501ef345595
2547technowizz <neil_edward_lobo@hotmail.com>
2020-04-18 21:47:37 -0400

    added routes

--- server.js
+++ server.js
@@ -7,5 +7,32 @@ var upload = multer();
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({extended:true}));
 app.use(upload.array());
+app.set("view engine", "pug");
+app.set("views", "./views")
 
 app.listen(process.env.PORT || 3000);
+
+app.get("/", function(req,res) {
+	res.render("login");
+});
+
+app.get("/chat", function(req,res) {
+	console.log(req.query);
+	try
+	{
+		if (req.query.name == "" || req.query.name == undefined)
+		{
+			throw Error
+		}
+		else
+		{
+			res.render("chat", {
+				name: req.query.name
+			});
+		}
+	}
+	catch(err)
+	{
+		res.redirect("/");
+	}
+})
